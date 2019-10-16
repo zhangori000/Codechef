5a454aabb7ae9445cd5c082ff2034bd6c708ddef
Orien Zhang <33295091+zhangori000@users.noreply.github.com>
2019-10-15 19:28:28 -0500

    Update WATCHGB.cpp

--- WATCHGB.cpp
+++ WATCHGB.cpp
@@ -1,6 +1,12 @@
 // search up september lunchtime 2019
 
-
+/* the way to solv ethis is to realize that there are several cases.
+obviously, when we have type == 1, we obviously know the score of both teams.
+but what if we have type two? well we would only know the scores IF...
+think about it this way. 
+we know current score is 2 vs 5
+and now we see that wow team A scored 6 points and team B scored 3 poits. well look. a team can't TAKE AWAY points
+*/
 #include<bits/stdc++.h>
 using namespace std;
 
